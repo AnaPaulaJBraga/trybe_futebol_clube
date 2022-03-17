0411f00f3ed969b074734653b773aa94e9402daf
AnaPaulaBraga <anapaula.junq@hotmail.com>
2022-03-17 19:40:39 -0300

    req 3 - migration e model users

---
+++ app/backend/src/database/models/users.ts
@@ -0,0 +1,43 @@
+import { Model, DataTypes } from 'sequelize';
+import db from '.';
+
+class Users extends Model {
+  public id: number;
+
+  public username: string;
+
+  public role: string;
+
+  public email: string;
+
+  public password: string;
+}
+
+Users.init({
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: DataTypes.INTEGER,
+  },
+  username: {
+    type: DataTypes.STRING,
+  },
+  role: {
+    type: DataTypes.STRING,
+  },
+  email: {
+    type: DataTypes.STRING,
+  },
+  password: {
+    type: DataTypes.STRING,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'Users',
+  tableName: 'users',
+  timestamps: false,
+});
+
+export default Users;
